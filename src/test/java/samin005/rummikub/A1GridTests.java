c0072451b8b78ab845e2880181c35f47169560ab
Samin Azhan <34473594+Samin005@users.noreply.github.com>
2021-09-28 20:18:12 +0600

    TEST for row 46

--- src/test/java/samin005/rummikub/A1GridTests.java
+++ src/test/java/samin005/rummikub/A1GridTests.java
@@ -47,5 +47,20 @@ public class A1GridTests {
         assertEquals(61, currentGame.getTilesRemaining().size());
         // ensuring board is empty
         assertEquals(0, currentGame.getBoard().size());
+
+        // ensuring current player is player 2
+        assertEquals(2, currentGame.getCurrentPlayer());
+        // ensuring player 2 has R11 R12 R13 before playing
+        assertTrue(player2.getInHand().toString().contains("R11, R12, R13"));
+        // ensuring the board does not contain the meld R11 R12 R13
+        assertFalse(currentGame.getBoard().toString().contains("R11, R12, R13"));
+        // player 2 playing the meld R11 R12 R13
+        gameService.executeTurn("play R11 R12 R13");
+        // ensuring player 2 does not have R11 R12 R13 anymore
+        assertFalse(player2.getInHand().toString().contains("R11, R12, R13"));
+        // ensuring the board contains the meld R11 R12 R13 now
+        assertTrue(currentGame.getBoard().toString().contains("R11, R12, R13"));
+        // as a new meld has been added to board, it's size should increase from 0 to 1
+        assertEquals(1, currentGame.getBoard().size());
     }
 }
