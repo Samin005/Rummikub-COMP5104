c6d78ff14d9fff288b14058bd23b54086c9a2c29
Samin Azhan <34473594+Samin005@users.noreply.github.com>
2021-10-24 20:18:36 +0600

    STEP COMMIT JokerReuse

--- src/test/java/samin005/rummikub/cucumber/StepDefinitions.java
+++ src/test/java/samin005/rummikub/cucumber/StepDefinitions.java
@@ -34,14 +34,23 @@ public class StepDefinitions {
     @Given("game starts")
     public void gameStarts() {
         gameService.startGame();
+
+        assertEquals(14, currentGame.getPlayerByNumber(1).getInHand().size());
+        assertEquals(14, currentGame.getPlayerByNumber(2).getInHand().size());
+        assertEquals(14, currentGame.getPlayerByNumber(3).getInHand().size());
+        assertEquals(64, currentGame.getTilesRemaining().size());
     }
 
     @When("player {int} plays {string}")
     public void playerPlays(int playerNo, String meld) {
         player = currentGame.getPlayerByNumber(playerNo);
-        // ensuring it the given playerNo's turn
-        assertEquals(playerNo, currentGame.getCurrentPlayer());
         gameService.executeTurn("play " + meld);
+    }
+
+    @When("player {int} executes {string}")
+    public void playerExecutes(int playerNo, String command) {
+        player = currentGame.getPlayerByNumber(playerNo);
+        gameService.executeTurn(command);
     }
 
     @And("player {int} ends turn")
@@ -58,6 +67,86 @@ public class StepDefinitions {
             assertEquals(0, currentGame.getBoard().size());
             // the player drew 3 tiles for an invalid move, so in hand tiles should be 14+3=17
             assertEquals(17, player.getInHand().size());
+        }
+        // valid move
+        else {
+            assertEquals(status, "valid");
+            // as melds were added to the board, the size should not remain 0
+            assertTrue(currentGame.getBoard().size() > 0);
+            // the player played melds and the minimum valid meld size is 3, so in hand tiles should be <= 14-3 = 11
+            assertTrue(player.getInHand().size() <= 11);
+        }
+    }
+
+    @Then("break board game status should be {string}")
+    public void breakBoardGameStatusShouldBe(String status) {
+        // invalid move
+        if(currentGame.getStatus().startsWith("INVALID")) {
+            assertEquals(status, "invalid");
+            // as no melds were added to the board, the size should remain 6
+            assertEquals(6, currentGame.getBoard().size());
+            // the player drew 3 tiles for an invalid move, so in hand tiles should be 11+3=14
+            assertEquals(14, player.getInHand().size());
+        }
+        // valid move
+        else {
+            assertEquals(status, "valid");
+            // as melds were added to the board, the size should not remain 6
+            assertTrue(currentGame.getBoard().size() != 6);
+            // the player played at least 1 valid tile from hand, so in hand tiles should be <11
+            assertTrue(player.getInHand().size() < 11);
+        }
+    }
+
+    @Then("add board game status should be {string}")
+    public void addBoardGameStatusShouldBe(String status) {
+        // invalid move
+        if(currentGame.getStatus().startsWith("INVALID")) {
+            assertEquals(status, "invalid");
+            // as no melds were added to the board, the size should remain 6
+            assertEquals(6, currentGame.getBoard().size());
+            // the player drew 3 tiles for an invalid move, so in hand tiles should be 11+3=14
+            assertEquals(14, player.getInHand().size());
+        }
+        // valid move
+        else {
+            assertEquals(status, "valid");
+            // as melds were added to the board, the size should not remain 6
+            assertTrue(currentGame.getBoard().size() == 6);
+            // the player played at least 1 valid tile from hand, so in hand tiles should be <11
+            assertTrue(player.getInHand().size() < 11);
+        }
+    }
+
+    @Then("merge board game status should be {string}")
+    public void mergeBoardGameStatusShouldBe(String status) {
+        // invalid move
+        if(currentGame.getStatus().startsWith("INVALID")) {
+            assertEquals(status, "invalid");
+            // as no melds were added to the board, the size should remain 5
+            assertEquals(6, currentGame.getBoard().size());
+            // the player drew 3 tiles for an invalid move, so in hand tiles should be 11+3=14
+            assertEquals(14, player.getInHand().size());
+        }
+        // valid move
+        else {
+            assertEquals(status, "valid");
+            // as melds were added to the board, the size should less than 6
+            assertTrue(currentGame.getBoard().size() < 6);
+            // the player played at least 1 valid tile from hand, so in hand tiles should be <11
+            assertTrue(player.getInHand().size() < 11);
+        }
+    }
+
+    @Then("joker reuse game status should be {string}")
+    public void jokerReuseGameStatusShouldBe(String status) {
+        // invalid move
+        if(currentGame.getStatus().startsWith("INVALID")) {
+            assertEquals(status, "invalid");
+            // as no new melds were added to the board, the size should remain 5
+            assertEquals(5, currentGame.getBoard().size());
+            // the player drew 3 tiles for an invalid move, so in hand tiles should be 11+3=14
+            assertEquals(14, player.getInHand().size());
         }
         // valid move
         else {
@@ -87,5 +176,24 @@ public class StepDefinitions {
             // the player played melds and the minimum valid meld size is 3, so in hand tiles should be <= 14-3 = 11
             assertTrue(player.getInHand().size() <= 11);
         }
+    }
+
+    @Then("game scores should be {string}, {string}, {string}, {string}")
+    public void gameScoresShouldBe(String p1Score, String p2Score, String p3Score, String status) {
+        if(status.equals("valid")) {
+            assertEquals(Integer.parseInt(p1Score), currentGame.getPlayerByNumber(1).getScore());
+            assertEquals(Integer.parseInt(p2Score), currentGame.getPlayerByNumber(2).getScore());
+            assertEquals(Integer.parseInt(p3Score), currentGame.getPlayerByNumber(3).getScore());
+        }
+        else {
+            assertNotEquals(Integer.parseInt(p1Score), currentGame.getPlayerByNumber(1).getScore());
+            assertNotEquals(Integer.parseInt(p2Score), currentGame.getPlayerByNumber(2).getScore());
+            assertNotEquals(Integer.parseInt(p3Score), currentGame.getPlayerByNumber(3).getScore());
+        }
+    }
+
+    @Given("player {int} draws tile {string}")
+    public void playerDrawsTile(int playerNo, String tile) {
+        gameService.drawSpecificTile(currentGame.getPlayerByNumber(playerNo), tile);
     }
 }
